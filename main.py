c58b16436384fcd96a027bd0a143bc531802654a
Nathanial Wilson <tenebris0011@users.noreply.github.com>
2021-06-12 02:29:01 -0500

    Update main.py
    Add config file for importing login info and card info.
    Add ability for script to input cvv code at checkout.

--- main.py
+++ main.py
@@ -1,61 +1,50 @@
-# This is a sample Python script.
-from bs4 import BeautifulSoup as bs
-import requests
-import pandas
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
-import pandas
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from configparser import ConfigParser
 
-def scraper():
-    url = "https://discuss.python.org/t/help-with-request-get-access-denied/6540"
-    req = requests.get(url)
-    soup = bs(req.content, 'html.parser')
+global file
+file = './assets/config.ini'
 
 def automateBestBuy():
     options = webdriver.ChromeOptions()
     # options.headless = True
     driver = webdriver.Chrome(executable_path='C:\\Users\\Nathan\\PycharmProjects\\webscraper\\assets\\chromedriver.exe', options=options)
     driver.maximize_window()
-    driver.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-ti-8gb-gddr6x-pci-express-4-0-graphics-card-dark-platinum-and-black/6465789.p?skuId=6465789")
-    # driver.get("https://www.bestbuy.com/site/apple-airpods-pro-white/5706659.p?skuId=5706659")
+    driver.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442")
+    # driver.get("https://www.bestbuy.com/site/dynex-5-w-usb-wall-charger-white/6403449.p?skuId=6403449")
 
     buyButton = False
 
-    # time.sleep(1)
-    sleep = time.sleep(1)
-
     while not buyButton:
         try:
-            time.sleep(1)
             addToCartBtn = addButton = driver.find_element_by_class_name("btn-disabled")
 
             print("Button isn't ready yet.")
 
-            time.sleep(5)
+            time.sleep(2)
             driver.refresh()
 
         except:
-            time.sleep(1)
             addToCartBtn = addButton = driver.find_element_by_class_name("btn-primary")
 
             print("Button was clicked.")
             addToCartBtn.click()
             buyButton = True
 
-            time.sleep(1)
+            time.sleep(2)
             driver.get("https://www.bestbuy.com/cart")
+
+            time.sleep(2)
             checkoutBtn = addButton = driver.find_element_by_class_name("btn-primary")
             checkoutBtn.click()
 
             time.sleep(2)
             elem = driver.find_element_by_class_name("tb-input ")
-            elem.send_keys("email")
+            elem.send_keys(account['email'])
             elem2 = driver.find_element_by_id("fld-p1")
-            elem2.send_keys("password")
+            elem2.send_keys(account['password'])
 
+            time.sleep(2)
             sgninBtn = addButton = driver.find_element_by_class_name("btn-secondary")
             sgninBtn.click()
 
@@ -63,23 +52,26 @@ def automateBestBuy():
             exitBtn = addButton = driver.find_element_by_class_name("btn-primary")
             exitBtn.click()
 
+            time.sleep(2)
             swithBtn = addButton = driver.find_element_by_class_name("ispu-card__switch")
             swithBtn.click()
 
-            plcOrdBtn = addButton = driver.find_element_by_class_name("btn-primary")
+            time.sleep(2)
+            btn = addButton = driver.find_element_by_id("credit-card-cvv")
+            btn.send_keys(card['cvv'])
+
+            time.sleep(2)
+            plcOrdBtn = addButton = driver.find_element_by_class_name("button__fast-track")
             plcOrdBtn.click()
 
             time.sleep(100000)
 
-
-    # driver.save_screenshot("test.png")
-
-    # time.sleep(5)
     driver.close()
 
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    config = ConfigParser()
+    config.read(file)
+    account = config['login']
+    card = config['card']
+
     automateBestBuy()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
